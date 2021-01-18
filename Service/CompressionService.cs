a002900ffa8d33328e5095b7e4a0382e5ea9bcf7
1586606701 <1586606701@qq.com>
2021-01-18 11:59:40 +0800

    CompressionService

--- Service/CompressionService.cs
+++ Service/CompressionService.cs
@@ -1,4 +1,6 @@
-﻿using MinecraftTunnel.Common;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MinecraftTunnel.Common;
 using MinecraftTunnel.Model.Protocol.ServerBound;
 using MinecraftTunnel.Protocol;
 using System;
@@ -7,6 +9,16 @@ namespace MinecraftTunnel.Service
 {
     public class CompressionService : IProtocol
     {
+        private readonly ILogger Logger;
+        private readonly TotalService TotalService;
+        private readonly IConfiguration Configuration;
+        public CompressionService(ILogger<CompressionService> Logger, IConfiguration Configuration, TotalService TotalService)
+        {
+            this.Logger = Logger;
+            this.Configuration = Configuration;
+            this.TotalService = TotalService;
+            this.Action = Auth;
+        }
         public SetCompression Instance { get; set; }
         public bool NeedAnalysis { get; set; } = false;
         public Action<PlayerToken, SetCompression> Action { get; set; }
